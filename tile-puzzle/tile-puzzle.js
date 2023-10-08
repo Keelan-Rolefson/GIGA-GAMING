89c345c6e6dfe8840d0c7dd0206e82af5c0d1392
Keelan-Rolefson <Keelanrolefson@gmail.com>
2023-10-08 16:37:09 -0500

    penis

---
+++ tile-puzzle/tile-puzzle.js
@@ -0,0 +1,15 @@
+
+let board = [
+    [10, 6, 3, 1],
+    [2, 14, 13, 8],
+    [4, 7, 5, 11],
+    [12, null, 9, 15]
+]
+
+const td = document.querySelector('td');
+td.addEventListener('click', e => {
+    const { target } = e;
+
+})
+
+console.log("penis");
