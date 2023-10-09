15765663bea29c32e417719663a1ff38030041e7
Keelan-Rolefson <Keelanrolefson@gmail.com>
2023-10-09 16:51:22 -0500

    position check, returning position of blank

--- tile-puzzle/tile-puzzle.js
+++ tile-puzzle/tile-puzzle.js
@@ -1,4 +1,5 @@
 
+// Board
 let board = [
     [10, 6, 3, 1],
     [2, 14, 13, 8],
@@ -6,38 +7,60 @@ let board = [
     [12, null, 9, 15]
 ]
 
+// Variables
 let currTileID = "";
 let currTileV = "";
+let posX = 0;
+let posY = 0;
 
+// Event listener
 const td = document.querySelector("table");
 td.addEventListener('click', e => {
     const { target } = e;
     currTileID = target.id;
     console.log(currTileID);
 
-    posCheck();
+    posY = Number(currTileID.substring(0,1));
+    posX = Number(currTileID.substring(2,3));
+
+    posCheck(posX, posY);
+    console.log(posCheck(posX, posY));
 
 })
 
-function posCheck() {
-    let checkUp = "";
-    let posY = Number(currTileID.substring(0,1));
-    let posX = Number(currTileID.substring(2,3));
+// Checking ID's of surrounding tiles, returning position of blank.
+function posCheck(posX, posY) {
 
-    if(posY === 0) {
-        checkRight = board[posY][posX + 1];
-        checkDown = board[posY + 1][posX];
-        checkLeft = board[posY][posX - 1];     
+    checkRight = board[posY][posX + 1];
+    checkLeft = board[posY][posX - 1];
+
+    if(checkLeft === null) {
+        return [posY, posX - 1];
     }
-    else {
-        checkRight = board[posY][posX + 1];
-        checkDown = board[posY + 1][posX];
-        checkLeft = board[posY][posX - 1];
+
+    if(checkRight === null) {
+        return [posY, posX + 1];
+    }
+
+    if(posY !== 0) {
         checkUp = board[posY - 1][posX];
+
+        if(checkUp === null) { 
+            return [posY - 1, posX];
+        }
     }
+
+    if(posY !== 3) {
+        checkDown = board[posY + 1][posX];
+        
+        if(checkDown === null) {
+            return [posY +1, posX];
+        }
+    }
+    
+    return false;
 }
 
-// checkUp = board[posY - 1][posX];
-// checkRight = board[posY][posX + 1];
-// checkDown = board[posY + 1][posX];
-// checkLeft = board[posY][posX - 1];
+function boardUpdate() {
+    
+}
